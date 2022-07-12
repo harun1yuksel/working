a184319795c60f2715d9f654f5968309de954517
harun1yuksel <harun1yuksel@gmail.com>
2022-07-13 00:38:10 +0300

    sql project

--- projects/sql_projects/NYC/solution_harun.sql
+++ projects/sql_projects/NYC/solution_harun.sql
@@ -32,12 +32,38 @@ order by Tip_amount DESC
 
 ---    4. Longest trip duration.
 
-SELECT top 1 *, Datediff(minute, [lpep_pickup_datetime], [Lpep_dropoff_datetime]) as trip_duration
+SELECT top 1 Datediff(minute, [lpep_pickup_datetime], [Lpep_dropoff_datetime]) as trip_duration, * 
 FROM sep_oct
 Order By (trip_duration) DESC
 
+---    5. Mean tip by hour.
+
+SELECT  distinct datepart(HOUR, [lpep_pickup_datetime]) as hour_in_day, 
+round(avg(Tip_amount) over(partition by datepart(HOUR, [lpep_pickup_datetime])),2,2) as avg_tip
+FROM sep_oct
+Order By 1
  
+---     6. Median trip cost (This question is optional. You can search for “median” calculation if you want).
 
+SELECT distinct PERCENTILE_DISC(0.5) WITHIN GROUP(Order by Total_amount) over() as median_amount
+from sep_oct
 
+/*    7. Average total trip by day of week (Fortunately, we have day of week information. 
+Otherwise, we need to create a new date column without hours from date column. 
+Then, we need to create "day of week" column, i.e Monday, Tuesday .. or 1, 2 ..,  from that new date column. 
+Total trip count should be found for each day, lastly average total trip should be calculated for each day). */
 
+SELECT distinct lpep_pickup_day_of_week, count(*) over(PARTITION by lpep_pickup_day_of_week) as trip_count
+from sep_oct
 
+SELECT distinct datepart(day,lpep_pickup_datetime) as day_of_month, lpep_pickup_day_of_week, 
+count(*) over(PARTITION by datepart(day,lpep_pickup_datetime) ) as trip_count
+from sep_oct
+
+with A as (
+    SELECT distinct datepart(day,lpep_pickup_datetime) as day_of_month, lpep_pickup_day_of_week, 
+    count(*) over(PARTITION by datepart(day,lpep_pickup_datetime) ) as trip_count
+    from sep_oct
+)
+SELECT distinct lpep_pickup_day_of_week, avg(trip_count) over (PARTITION by lpep_pickup_day_of_week) avg_trip_per_day
+From A 
